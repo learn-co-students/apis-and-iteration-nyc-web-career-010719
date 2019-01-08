73ca5da5cc290d04c62d75002d73d8edc7b73420
github username <github email address>
2019-01-08 15:41:24 -0500

    api and iteration solution minus the bonus

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,17 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars API!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  response = gets.chomp.downcase
+  return response
+end
+
+def get_movie_from_user
+  puts "please enter a movie title"
+  response = gets.chomp.downcase
+  return response
 end
