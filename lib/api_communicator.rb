bc1a4108ca90878d69b680588e6b8120aca937a7
github username <github email address>
2019-01-22 14:12:24 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,6 +7,22 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
+  films = []
+  response_hash["results"].each do |element|
+    if element["name"] == character_name
+      films = element["films"]
+    end
+  end
+# end
+
+  films.map do |film|
+    film_string = RestClient.get(film)
+    film_hash = JSON.parse(film_string)
+  end
+
+  # binding.pry
+  # "pray"
+
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,14 +35,20 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
+  films.each do |film|
+    puts film["title"]
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
+
 end
 
+ # binding.pry
+ # "end"
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
+  puts "welcome to the star wars app"
   # puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  character = gets.chomp
+  character
   # use gets to capture the user's input. This method should return that input, downcased.
 end
