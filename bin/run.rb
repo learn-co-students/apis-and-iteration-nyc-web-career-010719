1040f218d47c64181e3305bdfdbf06f5e2ef0ba0
Elijah Cleveland <elicleveland12@gmail.com>
2019-01-22 15:04:10 -0500

    The force is strong with this one

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,11 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+character_name = get_character_from_user
+get_character_movies_from_api(character_name)
+print_movies(character_name)
+film_id_num = get_film_from_user
+
+# binding.pry
+
+"whatever"
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
 def welcome
-  # puts out a welcome message here!
+  puts "May the force be with you"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  character_name = gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
+end
+
+def get_film_from_user
+  puts "which film do you want to know more about?"
+  episode_num = gets.chomp
 end
