886c04536b9d535ea2e10d68b0a5929c56d228b1
github username <github email address>
2019-01-22 14:17:07 -0500

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,33 +1,33 @@
-require 'rest-client'
-require 'json'
-require 'pry'
-
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
-end
-
-def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
-end
-
+require "JSON"
+require "RestClient"
+#It takes in an argument of a character's name (which gets passed to the method when it is called in the run.rb file) and relies on two helper methods that you will define to make a request to the API and parse the response from that request.
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+  films_array = get_character_movies_from_api(character)
+  print_movies(films_array)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+#get and parse for all
+def api_grabber(api_link)
+  response_string = RestClient.get(api_link)
+  response_hash = JSON.parse(response_string)
+end
+#So, the get_character_movies_from_api method should return an array of hashes, where each hash represents a movie.
+def get_character_movies_from_api(character)
+  my_films = nil
+  new_hash = api_grabber('http://www.swapi.co/api/people/')
+  character_info = new_hash["results"]
+  character_info.each do |characters|
+    if characters["name"] == character
+      my_films = characters["films"]
+    end
+  end
+  my_films
+end
+#This array of hashes is what gets passed to the print_movies method. This is the method in which you will iterate over that array of hashes to puts out movie information to the terminal.
+def print_movies(films_array)
+  all_my_films = []
+  films_array.each do |film|
+    newer_hash = api_grabber(film)
+    all_my_films << newer_hash
+  end
+  puts all_my_films
+end
